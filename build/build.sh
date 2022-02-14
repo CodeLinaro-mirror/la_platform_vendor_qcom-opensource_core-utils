6950a268bf7ec55ab2d83c1345a6ade27f3e3698
Sundhara Raja Usiripati <quic_susiripa@quicinc.com>
2022-02-14 10:13:19 +0530

    Revert "Build: Add qcs605 product to dynamic partition list."
    This reverts commit f23aaba737200b518c1ce0e2bc263ef405d06d84.
    
    Change-Id: I5f418df189b15101f9d77192fc8050389ef47ffe

--- build/build.sh
+++ build/build.sh
@@ -222,10 +222,9 @@ DIST_ENABLED=false
 QSSI_ARGS_WITHOUT_DIST=""
 DIST_DIR="out/dist"
 MERGED_TARGET_FILES="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-target_files.zip"
-LEGACY_TARGET_FILES="$DIST_DIR/${TARGET_PRODUCT}-target_files-*.zip"
 MERGED_OTA_ZIP="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-ota.zip"
-DIST_ENABLED_TARGET_LIST=("lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "trinket_32" "lito" "bengal" "bengal_32" "atoll" "bengal_32go" "qcs605")
-DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "trinket_32" "atoll" "bengal" "bengal_32" "bengal_32go" "qcs605")
+DIST_ENABLED_TARGET_LIST=("lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "trinket_32" "lito" "bengal" "bengal_32" "atoll" "bengal_32go")
+DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "trinket_32" "atoll" "bengal" "bengal_32" "bengal_32go")
 DYNAMIC_PARTITIONS_IMAGES_PATH=$OUT
 DP_IMAGES_OVERRIDE=false
 function log() {
@@ -415,20 +414,6 @@ function full_build () {
     merge_only
 }
 
-function nonqssi_legacy_build () {
-    command "source build/envsetup.sh"
-    if [ "$DP_IMAGES_OVERRIDE" = true ]; then
-       ARGS=${ARGS//"--dp_images_path=$DYNAMIC_PARTITIONS_IMAGES_PATH"/}
-    fi
-    command "make $ARGS"
-    if [ "$DIST_ENABLED" = true ] && [ "$BOARD_DYNAMIC_PARTITION_ENABLE" = true ]; then
-      check_if_file_exists "$DIST_DIR/super.img"
-      log "${TARGET_PRODUCT} copy $DIST_DIR/super.img to $OUT/ "
-      command "cp $DIST_DIR/super.img $OUT/"
-      command "unzip -jo -DD $LEGACY_TARGET_FILES IMAGES/*.img -x IMAGES/userdata.img -d $DYNAMIC_PARTITIONS_IMAGES_PATH"
-    fi
-}
-
 if [ "$TARGET_PRODUCT" == "qssi" ]; then
     log "FAILED: lunch option should not be set to qssi. Please set a target out of the following QSSI targets: ${QSSI_TARGETS_LIST[@]}"
     exit 1
@@ -446,8 +431,8 @@ done
 # For non-QSSI targets
 if [ $QSSI_TARGET_FLAG -eq 0 ]; then
     log "${TARGET_PRODUCT} is not a QSSI target. Using legacy build process for compilation..."
-    nonqssi_legacy_build
-
+    command "source build/envsetup.sh"
+    command "make $ARGS"
 else # For QSSI targets
     log "Building Android using build.sh for ${TARGET_PRODUCT}..."
     log "QSSI_ARGS=\"$QSSI_ARGS\""
