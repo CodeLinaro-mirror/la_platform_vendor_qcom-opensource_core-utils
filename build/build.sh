d36b19d5c3ce418355ca54a91d110f50acc69a6a
Eruvaram Kumar Raja Reddy <quic_erajared@quicinc.com>
2023-09-29 19:57:44 +0530

    build.sh: Enable msm8937_32 split lunch
    - Add msm8937_32 in qssi_32 mapping target product
    - Add msm8937_32 in build_image_standalone.py.
    - Add msm8937_32 in DIST_ENABLED_TARGET_LIST and
      DYNAMIC_PARTITION_ENABLED_TARGET_LIST
    
    Change-Id: Icb7e1501a884fac63ccc94be3aaef66ccb166281

--- build/build.sh
+++ build/build.sh
@@ -236,7 +236,7 @@ ENABLE_VIRTUAL_AB=false
 # use these lists to pair target lunch options with their corresponding qssi type.
 TARGET_PRODUCT_MAPPING_QSSI=("holi" "taro" "kalama" "lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "lito" "bengal" "qssi" "parrot" "bengal_515" "crow" "qcs605")
 TARGET_PRODUCT_MAPPING_QSSI_64=("kalama64" "pineapple" "qssi_64")
-TARGET_PRODUCT_MAPPING_QSSI_32=("bengal_32" "qssi_32")
+TARGET_PRODUCT_MAPPING_QSSI_32=("bengal_32" "qssi_32" "msm8937_32")
 TARGET_PRODUCT_MAPPING_QSSI_32GO=("bengal_32go" "qssi_32go" "msm8937_32go" "msm8937_lily")
 
 QSSI_TARGET_FLAG=1
@@ -264,9 +264,9 @@ DIST_DIR="out/dist"
 MERGED_TARGET_FILES="$DIST_DIR/merged-${TARGET_MATCHING_QSSI}_${TARGET_PRODUCT}-target_files.zip"
 LEGACY_TARGET_FILES="$DIST_DIR/${TARGET_PRODUCT}-target_files-*.zip"
 MERGED_OTA_ZIP="$DIST_DIR/merged-${TARGET_MATCHING_QSSI}_${TARGET_PRODUCT}-ota.zip"
-DIST_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "sdm660_64" "msm8937_lily" "msm8937_32go" "bengal_515" "monaco" "crow" "qcs605")
+DIST_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "sdm660_64" "msm8937_lily" "msm8937_32go" "msm8937_32" "bengal_515" "monaco" "crow" "qcs605")
 VIRTUAL_AB_ENABLED_TARGET_LIST=("kona" "lito" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "bengal_515" "monaco" "crow" "trinket" "qcs605")
-DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal" "bengal_32" "bengal_32go" "sm6150" "sdm660_64" "msm8937_lily" "msm8937_32go" "bengal_515" "monaco" "crow" "qcs605")
+DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal" "bengal_32" "bengal_32go" "sm6150" "sdm660_64" "msm8937_lily" "msm8937_32go" "msm8937_32"  "bengal_515" "monaco" "crow" "qcs605")
 DYNAMIC_PARTITIONS_IMAGES_PATH=$OUT
 DP_IMAGES_OVERRIDE=false
 TECHPACK_LIST=("camera_tp" "display_tp" "video_tp" "audio_tp" "sensors_tp" "cv_tp" "xr_tp")
