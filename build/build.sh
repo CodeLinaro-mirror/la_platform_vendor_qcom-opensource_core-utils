25cdd4d6270c52da3632e5a157fdaf5c7c375bf7
Rajesh RN <quic_rajeshra@quicinc.com>
2023-11-22 17:32:20 +0530

    Build : Add support for super image creation with msmnile_gvmq_s_u lunch combo
    Updated msmnile_gvmq_s_u target for qssi_au product map list,
    dist enabled ,AB, and dynamic partition supported products
    
    Change-Id: Id273b33cc7801cc6844451d578fe7b338aa80754
    Signed-off-by: Rajesh RN <quic_rajeshra@quicinc.com>

--- build/build.sh
+++ build/build.sh
@@ -240,7 +240,11 @@ TARGET_PRODUCT_MAPPING_QSSI=("holi" "taro" "kalama" "lahaina" "sdm710" "sdm845" 
 TARGET_PRODUCT_MAPPING_QSSI_64=("kalama64" "pineapple" "qssi_64")
 TARGET_PRODUCT_MAPPING_QSSI_32=("bengal_32" "qssi_32" "monaco")
 TARGET_PRODUCT_MAPPING_QSSI_32GO=("bengal_32go" "qssi_32go" "msm8937_lily")
+<<<<<<< HEAD   (49bd01 Merge bbf8ea5b8d702c5aea0eb4526f278a565a5025b5 on remote bra)
 TARGET_PRODUCT_MAPPING_QSSI_AU=("msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_gy" "qssi_au" "sm6150_au")
+=======
+TARGET_PRODUCT_MAPPING_QSSI_AU=("msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_microdroid" "gen4_gvm_gy" "qssi_au" "sm6150_au" "msmnile_gvmq_vcu" "msmnile_gvmq_s_u")
+>>>>>>> CHANGE (1d0665 Build : Add support for super image creation with msmnile_gv)
 
 QSSI_TARGET_FLAG=1
 # check if our TARGET_PRODUCT is in any of these lists
@@ -269,9 +273,15 @@ DIST_DIR="out/dist"
 MERGED_TARGET_FILES="$DIST_DIR/merged-${TARGET_MATCHING_QSSI}_${TARGET_PRODUCT}-target_files.zip"
 LEGACY_TARGET_FILES="$DIST_DIR/${TARGET_PRODUCT}-target_files-*.zip"
 MERGED_OTA_ZIP="$DIST_DIR/merged-${TARGET_MATCHING_QSSI}_${TARGET_PRODUCT}-ota.zip"
+<<<<<<< HEAD   (49bd01 Merge bbf8ea5b8d702c5aea0eb4526f278a565a5025b5 on remote bra)
 DIST_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "sdm660_64" "msm8937_lily" "bengal_515" "monaco" "msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_gy" "qssi_au" "sm6150_au")
 VIRTUAL_AB_ENABLED_TARGET_LIST=("kona" "lito" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "bengal_515" "msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_gy" "sm6150_au")
 DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal" "bengal_32" "bengal_32go" "sm6150" "sdm660_64" "msm8937_lily" "bengal_515" "monaco" "msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_gy" "qssi_au" "sm6150_au")
+=======
+DIST_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "sdm660_64" "msm8937_lily" "bengal_515" "monaco" "msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_microdroid" "gen4_gvm_gy" "qssi_au" "sm6150_au" "msmnile_gvmq_vcu" "msmnile_gvmq_s_u")
+VIRTUAL_AB_ENABLED_TARGET_LIST=("kona" "lito" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "bengal_515" "msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_microdroid" "gen4_gvm_gy" "sm6150_au" "msmnile_gvmq_vcu" "msmnile_gvmq_s_u")
+DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "kalama" "parrot" "kalama64" "pineapple" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_64" "qssi_32" "qssi_32go" "bengal" "bengal_32" "bengal_32go" "sm6150" "sdm660_64" "msm8937_lily" "bengal_515" "monaco" "msmnile_au" "msmnile_gvmq" "gen4_gvm" "gen4_gvm_microdroid" "gen4_gvm_gy" "qssi_au" "sm6150_au" "msmnile_gvmq_vcu" "msmnile_gvmq_s_u")
+>>>>>>> CHANGE (1d0665 Build : Add support for super image creation with msmnile_gv)
 DYNAMIC_PARTITIONS_IMAGES_PATH=$OUT
 DP_IMAGES_OVERRIDE=false
 TECHPACK_LIST=("camera_tp" "display_tp" "video_tp" "audio_tp" "sensors_tp" "cv_tp" "xr_tp")
