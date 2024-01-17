88c55ffbb15eebe16da8e48c773703a8e7e9d5f6
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-01-17 11:15:21 +0530

    Disable QIIFA Temporarily at Vendor Side
    Android 14 onwards, QIIFA starts checking HIDL,AIDLs at vendor side as
    well. QIIFA is not fully enabled yet due to dependencies on VPP HAL and
    qcarcam AIDL issues. Enable it back once QIIFA is enabled fully
    
    Signed-off-by: Ashok Mutyala <amutyala@qti.qualcomm.com>
    Change-Id: I04287436af4c80deac57a1502a68dd7f1fceaac2

--- build/build.sh
+++ build/build.sh
@@ -591,7 +591,6 @@ function build_target_only () {
         bash ghgvm-pilsplitter.sh
         log "PIL splitted images are created at $OUT/scratch"
     fi
-    command "run_qiifa"
 }
 
 function merge_only () {
