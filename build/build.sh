e38fb5ceef2c496f88b17b331a9533c74ca0e672
Venkata Manikumar Punyamanthula <quic_vpunyama@quicinc.com>
2024-10-17 19:59:31 -0700

    Revert "Enable Commonsys-intf Checker Script"
    This reverts commit ee44120428c6512efd8e4ad5c69a3ed02fcde542.
    
    Reason for revert: Builds are failing
    
    Change-Id: I226ed389563d05a9655c7688869da3d2bfb7ac62

--- build/build.sh
+++ build/build.sh
@@ -650,10 +650,10 @@ function build_qssi_only () {
     # command "python -B $QTI_BUILDTOOLS_DIR/build/makefile-violation-scanner.py"
     log "QSSI_ARGS is $QSSI_ARGS"
     command "make $QSSI_ARGS"
-    COMMONSYS_INTF_SCRIPT="$QTI_BUILDTOOLS_DIR/build/commonsys_intf_checker.py"
-    if [ -f $COMMONSYS_INTF_SCRIPT ];then
-      command "python -B $COMMONSYS_INTF_SCRIPT"
-    fi
+    #COMMONSYS_INTF_SCRIPT="$QTI_BUILDTOOLS_DIR/build/commonsys_intf_checker.py"
+    #if [ -f $COMMONSYS_INTF_SCRIPT ];then
+    #  command "python -B $COMMONSYS_INTF_SCRIPT"
+    #fi
 }
 
 function build_target_only () {
