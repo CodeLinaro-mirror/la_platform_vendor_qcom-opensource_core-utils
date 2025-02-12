b9d9b843b640514eac7ae10b86da70c1ea4a514e
Baji Patthan <quic_bpatthan@quicinc.com>
2025-02-11 20:46:40 -0800

    Revert "core-utils: enable 64-bit automotive qssi target"
    This reverts commit c9d39d206976a942377479b40333c9c0b7e5fd31.
    
    Reason for revert: As the HY11 build is failing , we are reverting this change temporarily to unblock other teams for other SIs
    
    Change-Id: Ica88a6f18906e5e0a1cb33c8a600ffacb1cebe79

--- build/build_image_standalone.py
+++ build/build_image_standalone.py
@@ -248,7 +248,7 @@ def build_superimage(temp_dir, qssi_build_path, target_build_path,
 
   # Run QIIFA checks to ensure these builds are compatible, before merging them.
   if not skip_qiifa:
-    if QSSI_TARGET == "qssi" or QSSI_TARGET == "qssi_64" or QSSI_TARGET == "qssi_au" or QSSI_TARGET == "qssi_au_64" :
+    if QSSI_TARGET == "qssi" or QSSI_TARGET == "qssi_64" or QSSI_TARGET == "qssi_au" :
       run_qiifa_checks(temp_dir, qssi_build_path, target_build_path, merged_build_path, target_lunch)
     else:
       logging.info("Skipping QIIFA checks for 32-bit and Go targets")
@@ -323,8 +323,7 @@ def main():
     "qssi_32"   : ["bengal_32", "monaco"],
     "qssi_32go" : ["bengal_32go", "msm8937_lily"],
     "qssi_64"   : ["kalama64", "pineapple"],
-    "qssi_au"   : ["msmnile_au", "msmnile_au_s_u", "msmnile_tb", "sm6150_au", "msmnile_gvmq", "gen4_gvm", "gen4_gvm_gy", "msmnile_gvmq_vcu", "msmnile_gvmq_s_u"],
-    "qssi_au_64"   : ["gen5_gvm_gy"],
+    "qssi_au"   : ["msmnile_au", "msmnile_au_s_u", "msmnile_tb", "sm6150_au", "msmnile_gvmq", "gen4_gvm", "gen4_gvm_gy", "msmnile_gvmq_vcu", "msmnile_gvmq_s_u", "gen5_gvm_gy"],
   }
 
   if args.target_lunch   in vendor_qssi_mapping_dict['qssi']:
@@ -337,8 +336,6 @@ def main():
     QSSI_TARGET="qssi_64"
   elif args.target_lunch in vendor_qssi_mapping_dict['qssi_au']:
     QSSI_TARGET="qssi_au"
-  elif args.target_lunch in vendor_qssi_mapping_dict['qssi_au_64']:
-    QSSI_TARGET="qssi_au_64"
   else:
     print("ERROR: Unrecognized target_lunch input. Need to add lunch option to the vendor_qssi_matching_dict")
     return
