a6e1fd7b0bdde75acc82bd92f6e7b43e5287d9c5
Roopesh Nataraja <quic_roopeshr@quicinc.com>
2025-04-03 14:31:15 -0700

    core-utils: Create soccp_firmware mount point at compile time
    Add support to create soccp_firmware mount point at compile time.
    
    Change-Id: I74bff3e8a5bc5c01d8dcc4db42782fe36436b740
    (cherry picked from commit 3b72faa14ab2ae29fdbe2f423fb2be90cf848db8)

--- build/AndroidBoardCommon.mk
+++ build/AndroidBoardCommon.mk
@@ -10,6 +10,7 @@ FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
 BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
 DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
 PERSIST_MOUNT_POINT := $(TARGET_ROOT_OUT)/persist
+SOCCP_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/soccp_firmware
 
 ifeq ($(TARGET_BOARD_PLATFORM),qssi)
  ifeq ($(TARGET_BOARD_SUFFIX),_64)
@@ -29,7 +30,8 @@ endif
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) \
 				 $(BT_FIRMWARE_MOUNT_POINT) \
-				 $(DSP_MOUNT_POINT)
+				 $(DSP_MOUNT_POINT) \
+				 $(SOCCP_FIRMWARE_MOUNT_POINT)
 
 $(FIRMWARE_MOUNT_POINT):
 	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
@@ -56,6 +58,13 @@ $(PERSIST_MOUNT_POINT):
 	@echo "Creating $(PERSIST_MOUNT_POINT)"
 ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
 	@ln -sf /mnt/vendor/persist $(TARGET_ROOT_OUT)/persist
+endif
+
+$(SOCCP_FIRMWARE_MOUNT_POINT):
+	@echo "Creating $(SOCCP_FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/soccp_firmware
+ifneq ($(TARGET_MOUNT_POINTS_SYMLINKS),false)
+	@ln -sf /vendor/soccp_firmware $(TARGET_ROOT_OUT)/soccp_firmware
 endif
 
 # Defining BOARD_PREBUILT_DTBOIMAGE here as AndroidBoardCommon.mk
