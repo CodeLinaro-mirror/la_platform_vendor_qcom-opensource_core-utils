f2299a49aed7909299be4e33efc758f65050585e
Srinivas Duddu <quic_sduddu.com>
2025-04-22 20:32:08 -0700

    Lunch: Modify build.sh to use release version set by lunch command
    Modify build.sh to use TARGET_RELEASE set by the lunch command
    without overriding it manually with hardcoded values.
    
    Change-Id: Ia3f515f9bc577f4ba7377c217001173f02e43542
    (cherry picked from commit 59f9472fbbbd839b497f45584ba4560807de716b)

--- build/build.sh
+++ build/build.sh
@@ -111,7 +111,9 @@
 #     Supports --techpack argument to build techpack target(s)
 #     Use options: --techpack <teckpack target(s)>
 #     Usage: ./build.sh dist --teckpack -j32 <teckpack target(s)>
-BUILD_SH_VERSION=16
+# Version 17:
+#     Build utilizes the TARGET_RELEASE set by the lunch without overriding.
+BUILD_SH_VERSION=17
 if [ "$1" == "--version" ]; then
     return $BUILD_SH_VERSION
     # Above return will work only if someone source'ed this script (which is expected, need to source the script).
@@ -630,12 +632,12 @@ function run_qiifa_dependency_checker() {
 
 function build_qssi_only () {
     command "source build/envsetup.sh"
-    if [ "$TARGET_RELEASE" = "next" ] || [ "$TARGET_RELEASE" = "trunk_food" ];then
-      command "lunch ${TARGET_PRODUCT}-${TARGET_RELEASE}-${TARGET_BUILD_VARIANT}"
+    if [ -n "$TARGET_RELEASE" ]; then
+    command "lunch ${TARGET_PRODUCT}-${TARGET_RELEASE}-${TARGET_BUILD_VARIANT}"
     else
-      command "lunch ${TARGET_PRODUCT}-${TARGET_BUILD_VARIANT}"
+    echo "error:No release config set for target; please set TARGET_RELEASE, using 'lunch <target>-<release>-<build_type>'"
+    exit 1
     fi
-
     # When --sandbox_debug is passed as arg to Bazel, inaccessibleHelperDir gets
     # created with limited perms. Because of which find command fails and leads
     # to build failure in vendor builds. Update the perms to make sure find cmd
@@ -668,12 +670,12 @@ function build_qssi_only () {
 
 function build_target_only () {
     command "source build/envsetup.sh"
-    if [ "$TARGET_RELEASE" = "next" ] || [ "$TARGET_RELEASE" = "trunk_food" ];then
-      command "lunch ${TARGET}-${TARGET_RELEASE}-${TARGET_BUILD_VARIANT}"
+    if [ -n "$TARGET_RELEASE" ]; then
+    command "lunch ${TARGET_PRODUCT}-${TARGET_RELEASE}-${TARGET_BUILD_VARIANT}"
     else
-      command "lunch ${TARGET}-${TARGET_BUILD_VARIANT}"
+    echo "error:No release config set for target; please set TARGET_RELEASE, using 'lunch <target>-<release>-<build_type>'"
+    exit 1
     fi
-
     # When --sandbox_debug is passed as arg to Bazel, inaccessibleHelperDir gets
     # created with limited perms. Because of which find command fails and leads
     # to build failure in vendor builds. Update the perms to make sure find cmd
@@ -806,11 +808,13 @@ function build_techpack_only () {
         done
     fi
     command "source build/envsetup.sh"
-    if [ "$TARGET_RELEASE" = "next" ] || [ "$TARGET_RELEASE" = "trunk_food" ];then
-      command "lunch ${TARGET}-${TARGET_RELEASE}-${TARGET_BUILD_VARIANT}"
+    if [ -n "$TARGET_RELEASE" ]; then
+    command "lunch ${TARGET_PRODUCT}-${TARGET_RELEASE}-${TARGET_BUILD_VARIANT}"
     else
-      command "lunch ${TARGET}-${TARGET_BUILD_VARIANT}"
+    echo "error:No release config set for target; please set TARGET_RELEASE, using 'lunch <target>-<release>-<build_type>'"
+    exit 1
     fi
+
     command "python -B $QTI_BUILDTOOLS_DIR/build/makefile-violation-scanner.py"
     QSSI_ARGS="$QSSI_ARGS SKIP_ABI_CHECKS=$SKIP_ABI_CHECKS"
     command "run_qiifa_initialization"
@@ -846,6 +850,14 @@ function build_techpack_only_non_qssi () {
     command "source build/envsetup.sh"
     command "make $ARGS"
 }
+
+
+
+# Check if TARGET_PRODUCT is defined
+if [ -z "$TARGET_PRODUCT" ]; then
+    echo "error:No target product; please set TARGET_PRODUCT, using 'lunch <target>-<release>-<build_type>'"
+    exit 1
+fi
 
 # For non-QSSI targets
 if [ $QSSI_TARGET_FLAG -eq 0 ]; then
