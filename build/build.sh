edcd3b73b0516f1eb0bebbb64f3217f644423854
Raja Kumar Grandhi <rajagran@qti.qualcomm.com>
2025-06-19 02:55:24 -0700

    new lunch changes added for bengal_auto
    Change-Id: If51fc691a1722e269c644c71422f1054be2a840d

--- build/build.sh
+++ build/build.sh
@@ -155,7 +155,7 @@ if [[ "$TARGET_PRODUCT" == "qssi" ]]; then
     QSSI_ONLY=1
 fi
 
-QSSI_TARGETS_LIST=("holi" "taro" "lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "lito" "bengal" "bengal_2w" "bengal_2w_v2" "qssi" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "msm8937_lily" "msm8937_32go" "msm8937_32")
+QSSI_TARGETS_LIST=("holi" "taro" "lahaina" "sdm710" "sdm845" "msmnile" "sm6150" "kona" "atoll" "trinket" "lito" "bengal" "bengal_2w" "bengal_2w_v2" "bengal_auto" "qssi" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "msm8937_lily" "msm8937_32go" "msm8937_32")
 QSSI_TARGET_FLAG=0
 SKIP_ABI_CHECKS=true
 
@@ -217,9 +217,9 @@ DIST_DIR="out/dist"
 MERGED_TARGET_FILES="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-target_files.zip"
 LEGACY_TARGET_FILES="$DIST_DIR/${TARGET_PRODUCT}-target_files-*.zip"
 MERGED_OTA_ZIP="$DIST_DIR/merged-qssi_${TARGET_PRODUCT}-ota.zip"
-DIST_ENABLED_TARGET_LIST=("holi" "taro" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "bengal_2w" "bengal_2w_v2" "atoll" "qssi" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "msm8937_32go" "msm8937_32" "msm8937_64" "msm8953_32" "msm8953_64" "sdm429w_law" "sdm429w" "msm8937_lily" "monaco_go" "monaco" "monaco_go_aon")
+DIST_ENABLED_TARGET_LIST=("holi" "taro" "lahaina" "kona" "sdm710" "sdm845" "msmnile" "sm6150" "trinket" "lito" "bengal" "bengal_2w" "bengal_2w_v2" "bengal_auto" "atoll" "qssi" "qssi_32" "qssi_32go" "bengal_32" "bengal_32go" "msm8937_32go" "msm8937_32" "msm8937_64" "msm8953_32" "msm8953_64" "sdm429w_law" "sdm429w" "msm8937_lily" "monaco_go" "monaco" "monaco_go_aon")
 VIRTUAL_AB_ENABLED_TARGET_LIST=("kona" "lito" "taro" "lahaina" "monaco")
-DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_32" "qssi_32go" "bengal" "bengal_2w" "bengal_2w_v2" "bengal_32" "bengal_32go" "sm6150" "msm8937_32go" "msm8937_32" "msm8937_64" "msm8953_32" "msm8953_64" "sdm429w_law" "sdm429w" "msm8937_lily" "monaco_go" "monaco" "monaco_go_aon")
+DYNAMIC_PARTITION_ENABLED_TARGET_LIST=("holi" "taro" "lahaina" "kona" "msmnile" "sdm710" "lito" "trinket" "atoll" "qssi" "qssi_32" "qssi_32go" "bengal" "bengal_2w" "bengal_2w_v2" "bengal_auto" "bengal_32" "bengal_32go" "sm6150" "msm8937_32go" "msm8937_32" "msm8937_64" "msm8953_32" "msm8953_64" "sdm429w_law" "sdm429w" "msm8937_lily" "monaco_go" "monaco" "monaco_go_aon")
 DYNAMIC_PARTITIONS_IMAGES_PATH=$OUT
 DP_IMAGES_OVERRIDE=false
 
