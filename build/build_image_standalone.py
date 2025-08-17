db016b7f1c6ee57fcf14356f94a1354ad81e6395
Houffaneh Osman <hosman@qti.qualcomm.com>
2025-08-17 08:25:16 +0000

    build.sh: Adding gen3_gvm_coqos to the build targets for super image
    Change-Id: I56382b9710f799caa7d6099cdfade45a7a099ada
    Signed-off-by: Houffaneh Osman <hosman@qti.qualcomm.com>

--- build/build_image_standalone.py
+++ build/build_image_standalone.py
@@ -323,7 +323,7 @@ def main():
     "qssi_32"   : ["bengal_32", "monaco"],
     "qssi_32go" : ["bengal_32go", "msm8937_lily"],
     "qssi_64"   : ["kalama64", "pineapple"],
-    "qssi_au"   : ["msmnile_au", "msmnile_au_s_u", "msmnile_tb", "sm6150_au", "msmnile_gvmq", "gen4_gvm", "gen4_gvm_gy", "msmnile_gvmq_vcu", "msmnile_gvmq_s_u", "gen5_gvm_gy"],
+    "qssi_au"   : ["msmnile_au", "msmnile_au_s_u", "msmnile_tb", "sm6150_au", "msmnile_gvmq", "gen4_gvm", "gen4_gvm_gy", "msmnile_gvmq_vcu", "msmnile_gvmq_s_u", "gen5_gvm_gy", "gen3_gvm_coqos"],
   }
 
   if args.target_lunch   in vendor_qssi_mapping_dict['qssi']:
