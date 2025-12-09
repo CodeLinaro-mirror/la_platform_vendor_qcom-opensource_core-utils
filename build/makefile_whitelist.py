1322694f7157135df8f947ba53e2ce5931d7525e
Sai Kumar Kurapati <kurasaik@qti.qualcomm.com>
2025-12-09 12:13:21 +0530

    HGY.4.1.8.0 gen4_gvm_gy_sgt_lunch_support
    Change-Id: I492a9e7c0781671b82bb03e53d0ec1a7f1807a80

--- build/makefile_whitelist.py
+++ build/makefile_whitelist.py
@@ -164,6 +164,7 @@ LOCAL_COPY_HEADERS_WHITELIST = {
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_sgt/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_gy/Android.mk",
+    "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_gy_sgt/Android.mk"
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_gy_ccu/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen5_gvm_gy/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen5_gvm_sgt/Android.mk",
