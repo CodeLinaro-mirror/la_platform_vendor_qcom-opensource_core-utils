0d6a26d6bf8b37538f02c533f67b192d7f665011
Jakub Lecki <jlecki@qti.qualcomm.com>
2026-04-16 17:11:42 +0200

    Whitelist prebuilt_HY11 config for prime_gvm target.
    Change-Id: I8adbdcc122e9ebd13ee556209ae0762fa33799a0

--- build/makefile_whitelist.py
+++ build/makefile_whitelist.py
@@ -167,6 +167,7 @@ LOCAL_COPY_HEADERS_WHITELIST = {
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_gy/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_gy_sgt/Android.mk"
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen4_gvm_gy_ccu/Android.mk",
+    "vendor/qcom/proprietary/prebuilt_HY11/target/product/prime_gvm/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen5_gvm_gy/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11/target/product/gen5_gvm_sgt/Android.mk",
     "vendor/qcom/proprietary/prebuilt_HY11-HWASAN/target/product/msmnile_au/Android.mk",
